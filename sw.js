abe8b8971d06d5f29fca35e5eee14fdf2d0abd77
gera2ld <gera2ld@163.com>
2019-12-20 13:03:02 +0000

    Deploying to master from source 227c0f47c5eb26fc095f2de1468b782d425f30e6

--- sw.js
+++ sw.js
@@ -27,26 +27,26 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5b8ea3081b96bf3e9ce3.js"
+    "url": "webpack-runtime-cd510e5381c4965abad1.js"
   },
   {
     "url": "styles.9a58f0207df8f0e82447.css"
   },
   {
-    "url": "styles-43812c17a3b4fe9b1e4f.js"
+    "url": "styles-90a21c7d5dcca1cb7592.js"
   },
   {
-    "url": "commons-6f4029cca9d542290a4d.js"
+    "url": "commons-38541ebcd95a54413149.js"
   },
   {
-    "url": "app-63145a0d29132b20dd30.js"
+    "url": "app-22c67b38b576247c251b.js"
   },
   {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-5efb7b8fb8a6286e0a5c.js"
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-87ef582ba388a83f73c0.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "228d1a8c77c119a8a6670d4e109f7853"
+    "revision": "0390414d5faa1573a073b712e5fad1d0"
   },
   {
     "url": "manifest.webmanifest",
@@ -143,7 +143,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-63145a0d29132b20dd30.js`))) {
+  if (!resources || !(await caches.match(`/app-22c67b38b576247c251b.js`))) {
     return await fetch(event.request)
   }
 
