c25bb1621f04f6080326b1c5d1b6970e446ccb95
gera2ld <gera2ld@163.com>
2020-02-05 16:22:15 +0000

    Deploying to master from source - e7e0822ec136b663403066ba206ad39fe16fb54c 🚀

--- sw.js
+++ sw.js
@@ -27,26 +27,26 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-037cca19f23c7f98f00d.js"
+    "url": "webpack-runtime-5db6798fe6851e6ca1e7.js"
   },
   {
-    "url": "styles.9977294d25cc31402a65.css"
+    "url": "styles.dc6e3868014fa95c8b24.css"
   },
   {
     "url": "styles-dacf62430d73fb4fd695.js"
   },
   {
-    "url": "commons-fb65af8cc73c067368fa.js"
+    "url": "commons-e235e45ad07b55623a09.js"
   },
   {
-    "url": "app-4fa93a3e85d27d2ca025.js"
+    "url": "app-3518e94511c219b91385.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-cf2fd0f92da7499f18b1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "64482d36e0c6db4d5e4df7fe4ecb411b"
+    "revision": "18e5d7e307f230463cffd44c3e6aa8de"
   },
   {
     "url": "manifest.webmanifest",
@@ -143,7 +143,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-4fa93a3e85d27d2ca025.js`))) {
+  if (!resources || !(await caches.match(`/app-3518e94511c219b91385.js`))) {
     return await fetch(event.request)
   }
 
